9e05e2b278d5af0cfca0b22333b011c7f04af9f6
Ethan Nel <cethannel@protonmail.com>
2025-02-11 13:46:04 -0800

    Setup initial project

--- src/main/java/com/ethannel/csgta/CSGTA.java
+++ src/main/java/com/ethannel/csgta/CSGTA.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.ethannel.csgta;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,30 +10,33 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "MyMod", acceptedMinecraftVersions = "[1.7.10]")
-public class MyMod {
+@Mod(modid = CSGTA.MODID, version = Tags.VERSION, name = "CSGTA", acceptedMinecraftVersions = "[1.7.10]")
+public class CSGTA {
 
-    public static final String MODID = "mymodid";
+    public static final String MODID = "csgta";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.myname.mymodid.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
+    @SidedProxy(clientSide = "com.ethannel.csgta.ClientProxy", serverSide = "com.ethannel.csgta.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // preInit "Run before anything else. Read your config, create blocks, items,
+    // etc, and register them with the
     // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
     }
 
     @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    // load "Do your mod setup. Build whatever data structures you care about.
+    // Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
         proxy.init(event);
     }
 
     @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    // postInit "Handle interaction with other mods, complete your setup based on
+    // this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
     }
--- src/main/java/com/ethannel/csgta/Config.java
+++ src/main/java/com/ethannel/csgta/Config.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.ethannel.csgta;
 
 import java.io.File;
 
